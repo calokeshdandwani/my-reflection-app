b2165525cfab11ac218a876bc6672171ce10aaef
[dyad] <git@dyad.sh>
2025-07-20 00:49:28 +0530

    [dyad] Provided the SQL query for adding the parent_id column again, ensuring it's easy to copy, and reminded the user to refresh the app. - wrote 1 file(s)

---
+++ supabase_schema.sql
@@ -0,0 +1,8 @@
+-- Add parent_id column to tasks table if it doesn't exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='tasks' AND column_name='parent_id') THEN
+        ALTER TABLE public.tasks ADD COLUMN parent_id UUID REFERENCES public.tasks(id) ON DELETE CASCADE;
+    END IF;
+END
+$$;
